238fd240a7d5c17a2faad3504e67d8f02dfd3ebe
Alexander Chistyakov <al.ol.chistyakov@gmail.com>
2014-03-30 12:47:01 +0400

    changed the typo for fahr from float to int

--- temperature_reverse.c
+++ temperature_reverse.c
@@ -2,8 +2,7 @@
 
 main()
 {
-    float fahr;
-    int celsius;
+    int celsius, fahr;
     int lowerCelsius, upperCelsius, stepCelsius;
 
     lowerCelsius = 0;
@@ -11,12 +10,12 @@ main()
     stepCelsius = 20;
 
     celsius = lowerCelsius;
-    printf("%3s %6s\n", "C", "F");
+    printf("%3s %4s\n", "C", "F");
     printf("==========\n");
     while(celsius <= upperCelsius) 
     {
-        fahr = (9.0/5.0)*celsius + 32.0;
-        printf("%3d %6.1f\n", celsius, fahr);
+        fahr = celsius/5*9 + 32;
+        printf("%3d %4d\n", celsius, fahr);
         celsius = celsius + stepCelsius;
     }
 }
