66c31795fa29fb4b9c8dbd22d06d2e2116a1c24c
Alexander Chistyakov <al.ol.chistyakov@gmail.com>
2014-04-01 18:48:56 +0400

    ex. 1.9

--- ex19.c
+++ ex19.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 
+#define TRUE 1
+#define FALSE 0
+
 main()
 {
-    putchar('a');
-    putchar('\b');
-    putchar('c');
+    int currentChar;
+    int totalFollowingSpaces = 0;
+    char lineHasOnlySpaces = TRUE;
+    while((currentChar = getchar()) != EOF)
+    {
+        if(currentChar == ' ')
+        {
+            ++totalFollowingSpaces;
+            continue;
+        }
+        if(currentChar == '\n')
+        {
+            if(lineHasOnlySpaces & totalFollowingSpaces > 0)
+            {
+                putchar(' ');
+                totalFollowingSpaces = 0;
+                continue;
+            }
+            lineHasOnlySpaces = TRUE;
+        }
+        else
+            lineHasOnlySpaces = FALSE;
+        int i;
+        for(i = 0; i < totalFollowingSpaces; ++i)
+            putchar(' ');
+        putchar(currentChar);
+
+        totalFollowingSpaces = 0;
+    }
 }
 
